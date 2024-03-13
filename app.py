f4d4c15e7d67f4589a7c155a0490d4a3bf0773ed
kokamoto46 <119825792+kokamoto46@users.noreply.github.com>
2024-03-13 21:29:21 +0900

    Update app.py

--- app.py
+++ app.py
@@ -20,17 +20,32 @@ with st.sidebar:
     masking_noise_level = st.slider('Masking noise level (dBHL)', -5, 110, step=5)
 
 # Logic to light up the "response available" lamp based on the selected frequency and levels
-response_message = "No response"
-if frequency == 250 and pure_tone_level >= 50:
-    response_message = 'Response!'
-elif frequency == 500 and pure_tone_level >= 95 and masking_noise_level < 40:
-    response_message = 'Response!'
-elif frequency == 1000 and pure_tone_level >= 90 and masking_noise_level < 30:
-    response_message = 'Response!'
-elif frequency == 2000 and pure_tone_level >= 90:
-    response_message = 'Response!'
-elif frequency == 4000 and pure_tone_level >= 95:
-    response_message = 'Response!'
+response_message = ""
+if frequency == 250:
+    if pure_tone_level >= 50:
+        response_message = 'Response!'
+    else:
+        response_message = 'No response'
+elif frequency == 500:
+    if pure_tone_level >= 95 and masking_noise_level < 40:
+        response_message = 'Response!'
+    else:
+        response_message = 'No response'
+elif frequency == 1000:
+    if pure_tone_level >= 90 and masking_noise_level < 30:
+        response_message = 'Response!'
+    else:
+        response_message = 'No response'
+elif frequency == 2000:
+    if pure_tone_level >= 90:
+        response_message = 'Response!'
+    else:
+        response_message = 'No response'
+elif frequency == 4000:
+    if pure_tone_level >= 95:
+        response_message = 'Response!'
+    else:
+        response_message = 'No response'
 
 st.sidebar.success(response_message)
 
