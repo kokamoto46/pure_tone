75a86b94e8edc4b47b3aab7c542fbdc97a11a893
kokamoto46 <119825792+kokamoto46@users.noreply.github.com>
2024-03-13 21:52:27 +0900

    Update app.py

--- app.py
+++ app.py
@@ -15,37 +15,42 @@ with st.sidebar:
     # Dropdown menu for selecting the frequency
     frequency = st.selectbox('Frequency to be measured', [250, 500, 1000, 2000, 4000])
 
+    # Adjusting sliders based on the frequency selection
+    if frequency == 500:
+        max_level = 90
+    else:
+        max_level = 110
+
     # Sliders for adjusting the pure tone level and masking noise level
-    pure_tone_level = st.slider('Pure tone level (dBHL)', -5, 110, step=5)
-    masking_noise_level = st.slider('Masking noise level (dBHL)', -5, 110, step=5)
+    pure_tone_level = st.slider('Pure tone level (dBHL)', -5, max_level, step=5)
+    masking_noise_level = st.slider('Masking noise level (dBHL)', -5, max_level, step=5)
 
 # Logic to light up the "response available" lamp based on the selected frequency and levels
-response_message = ""
-if frequency == 250:
-    if pure_tone_level >= 50:
-        response_message = 'Response!'
-    else:
-        response_message = 'No response'
+response_message = "No response"
+if frequency == 250 and pure_tone_level >= 50:
+    response_message = 'Response!'
 elif frequency == 500:
-    if pure_tone_level >= 95 and masking_noise_level < 40:
+    if -20 <= masking_noise_level <= 35 and pure_tone_level >= 95:
         response_message = 'Response!'
-    else:
-        response_message = 'No response'
+    elif masking_noise_level == 40 and pure_tone_level >= 100:
+        response_message = 'Response!'
+    elif masking_noise_level == 45 and pure_tone_level >= 105:
+        response_message = 'Response!'
+    elif masking_noise_level >= 50 and pure_tone_level >= 110:
+        response_message = 'Response!'
 elif frequency == 1000:
-    if pure_tone_level >= 90 and masking_noise_level < 30:
+    if -20 <= masking_noise_level <= 30 and pure_tone_level >= 90:
         response_message = 'Response!'
-    else:
-        response_message = 'No response'
-elif frequency == 2000:
-    if pure_tone_level >= 90:
+    elif masking_noise_level == 35 and pure_tone_level >= 95:
         response_message = 'Response!'
-    else:
-        response_message = 'No response'
-elif frequency == 4000:
-    if pure_tone_level >= 95:
+    elif masking_noise_level == 40 and pure_tone_level >= 100:
         response_message = 'Response!'
-    else:
-        response_message = 'No response'
+    elif masking_noise_level >= 45 and pure_tone_level >= 105:
+        response_message = 'Response!'
+elif frequency == 2000 and pure_tone_level >= 90:
+    response_message = 'Response!'
+elif frequency == 4000 and pure_tone_level >= 95:
+    response_message = 'Response!'
 
 st.sidebar.success(response_message)
 
